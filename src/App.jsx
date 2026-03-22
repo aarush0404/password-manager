65a68a75dc0e4a6706cf9a1f09cb48ecb90e29f9
aarush0404 <coolart36542@gmail.com>
2026-03-22 15:15:29 +0530

    tailwind css working

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white p-6">
-      <h1 className="text-4xl font-bold text-center mb-8">
-        🔐 Password Manager
-      </h1>
+   <>
+ <h1 className="bg-amber-300">hello</h1>
 
-      <div className="max-w-md mx-auto bg-gray-800 p-6 rounded-lg shadow-lg">
-        <input
-          type="text"
-          placeholder="Website"
-          className="w-full mb-4 p-2 rounded bg-gray-700"
-        />
-
-        <input
-          type="text"
-          placeholder="Username"
-          className="w-full mb-4 p-2 rounded bg-gray-700"
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full mb-4 p-2 rounded bg-gray-700"
-        />
-
-        <button className="w-full bg-blue-500 hover:bg-blue-600 p-2 rounded">
-          Save Password
-        </button>
-      </div>
-    </div>
+   </>
   );
 }
 
